5c660ba345310dda3df84e6e4554e510ec54c19f
TieuLenh <hieukaito2k5@gmail.com>
2025-09-29 17:17:22 +0700

    assets solution

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,22 @@ module.exports = {
     },
     module: {
         rules: [
-        { test: /.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
-        { test: /.css$/, use: ["style-loader", "css-loader"] },
+            { test: /.(js|jsx)$/, exclude: /node_modules/, use: "babel-loader" },
+            { test: /.css$/, use: ["style-loader", "css-loader"] },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/images/[name][hash][ext][query]"
+                }
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)$/i,
+                type: "asset/resource",
+                generator: {
+                    filename: "assets/fonts/[name][hash][ext][query]"
+                }
+            }
         ],
     },
     plugins: [
