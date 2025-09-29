0dc52a033fc0c028b50929ad442d55999263b707
TieuLenh <hieukaito2k5@gmail.com>
2025-09-29 18:00:21 +0700

    completed configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import Header from "./components/header/header";
+import Slider from "./components/slider/slider";
+import Footer from "./components/footer/footer";
+
 const App = () => {
 	return(
         <>
             <Header />
+            <Slider />
+            <Footer />
         </>
     )
 }
