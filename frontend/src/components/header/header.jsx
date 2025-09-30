0259b92e7bf5ec046f8774ac8e839f5d73c35d48
TieuLenh <hieukaito2k5@gmail.com>
2025-09-30 21:45:07 +0700

    call api at header

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -3,26 +3,27 @@ import { useEffect, useState } from 'react';
 
 
 const Header = ({className = 'header', ...props}) => {
-    const [data, setData] = useState((addr = '/api') => {
-            async function getData(addr) {
-            try {
-                const res = await fetch(addr);   // đợi fetch xong
-                const data = await res.json();           // đợi parse xong
-                return data;
-            } catch (err) {
-                console.error(err);
-            }
-            }
-            return getData();
+    const [data, setData] = useState({});
+    useEffect(() => {
+        const fetchData = async () => {
+        try {
+            const res = await fetch('/api');
+            const json = await res.json();
+            setData(json);
+        } catch (err) {
+            console.error(err);
         }
-    ) 
+        };
+
+        fetchData();
+    }, []);
     return(
         <div
             className={className}
             {...props}
         >
             <ul>
-                {(data|| []).map((val, idx) => <li key={idx}><a href={'#'+val}>nav{idx}</a></li>)}
+                {(data.idList|| []).map((val, idx) => <li key={idx}><a href={'#'+val}>nav{idx}</a></li>)}
             </ul>
         </div>
     )
--- frontend/src/components/slider/slider.jsx
+++ frontend/src/components/slider/slider.jsx
@@ -11,9 +11,9 @@ const Slider = ({className = 'slider', ...props}) => {
             <div id='clock-container'>
                 <Clock />
             </div>
-            <div id='bar1' style={{height: '1000px',}}>content1</div>
-            <div id='bar2' style={{height: '1000px',}}>content2</div>
-            <div id='bar3' style={{height: '1000px',}}>content3</div>
+            <div id='nav1' style={{height: '1000px',}}>content1</div>
+            <div id='nav2' style={{height: '1000px',}}>content2</div>
+            <div id='nav3' style={{height: '1000px',}}>content3</div>
         </div>
     )
 }
