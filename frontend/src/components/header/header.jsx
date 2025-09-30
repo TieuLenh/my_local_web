7d5ec3e5a33684379ea37489ceb76f57599ddaa6
TieuLenh <hieukaito2k5@gmail.com>
2025-09-30 20:46:34 +0700

    css base

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -1,3 +1,4 @@
+import './Header.scss';
 const Header = ({className = 'header', ...props}) => {
     return(
         <div
@@ -5,6 +6,7 @@ const Header = ({className = 'header', ...props}) => {
             {...props}
         >
             <ul>
+                <li><a href="#clock-container">Home</a></li>
                 <li><a href="#bar1">bar1</a></li>
                 <li><a href="#bar2">bar2</a></li>
                 <li><a href="#bar3">bar3</a></li>
